93441c0daaf0ab1e672ef574f2d1f83764a2ec73
Rafael <rafael@uniondigital.co.nz>
2017-07-14 15:34:20 +1200

    configuring the Admin menu

--- src/app/admin/adminMenu/admin-menu.component.ts
+++ src/app/admin/adminMenu/admin-menu.component.ts
@@ -1,8 +1,22 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {UserService} from '../adminShared/user.service';
+import {Router} from '@angular/router';
 
 @Component({
     templateUrl:'./admin-menu.component.html',
     styleUrls: ['./admin-menu.component.css']
 })
 
-export class AdminMenuComponent{}
+export class AdminMenuComponent implements OnInit{
+    theUser: string;
+    constructor(private userSVC: UserService, private touter: Router){ }
+
+       ngOnInit(){
+            this.theUser = this.userSVC.loggedInUser;
+        }
+
+      /*  logout(){
+            thi
+        }*/
+
+}
